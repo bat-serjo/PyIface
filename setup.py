6851d6b870f8ad5f93d19a8cb98a013ecd53c098
Yegor Yefremov <yegorslists@googlemail.com>
2018-09-04 08:16:14 +0200

    Switch to setuptools_scm for specifying PyIface version
    Signed-off-by: Yegor Yefremov <yegorslists@googlemail.com>

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='pyiface',
-    version='0.0.9',
     author='Sergey Kostov',
     author_email='bat.serjo@gmail.com',
     packages=['pyiface'],
@@ -13,4 +12,6 @@ setup(
     description='View and control network interfaces. Linux only currently! Join us lets make it available for other OSes',
     long_description=open('README.rst').read(),
     install_requires=[],
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
 )
